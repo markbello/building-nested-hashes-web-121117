fa6b4abb74a70261995659c0ef9f302646884876
Mark Bello <mcbello51286@gmail.com>
2017-11-30 19:57:19 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -38,6 +38,6 @@ def fourth_challenge
    }
 }
 
-epic_tragedy[:montague][:hero][:status], epic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
 
 end
